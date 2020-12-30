c4e2a17130f07890da22cbcfe971dc65c0da565b
Gimna Katugampala <gimnakatugampala1@gmail.com>
2020-12-31 02:28:16 +0530

    init

---
+++ main.js
@@ -0,0 +1,131 @@
+//JS SDK
+window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '404843883907130',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v9.0'
+    });
+    
+    //get the user status
+    
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+
+  };
+
+  //Check if the user is logged in or not
+function statusChangeCallback(response){
+    console.log(response)
+    if(response.status === 'connected'){
+        console.log('User is Connected');
+        setElements(true)
+        testAPI();
+    }else{
+        console.log('User is not Authorized')
+    }
+}
+
+//Chek if the user is logged in
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+
+  //Call the data
+  function testAPI(){
+    FB.api('/me?fields=id,birthday,name,email,gender,posts,photos,videos,location,hometown,link', function(response){
+      if(response && !response.error){
+            buildProfile(response);
+            buildFeed(response);
+            showAlert(`Welcome ${response.name} !`)
+      }
+    })
+  }
+
+  //Show or Hide Elements
+  function setElements(isLoggedIn){
+    if(isLoggedIn){
+        document.querySelector('.login').style.display = 'none';
+        document.getElementById('profile').style.display = 'block';
+        document.getElementById('feed').style.display = 'block';
+        document.getElementById('logout').style.display = 'block';
+    }else{
+        document.querySelector('.login').style.display = 'block';
+        document.getElementById('profile').style.display = 'none';
+        document.getElementById('feed').style.display = 'none';
+        document.getElementById('logout').style.display = 'none';
+    }
+  }
+
+  //DOM Profile
+  function buildProfile(user){
+      let profile = `    
+      <div class="card mb-3">
+      <h3 class="card-header">Your Account Info</h3>
+      <ul class="list-group list-group-flush">
+          <li class="list-group-item">Name : ${user.name}</li>
+          <li class="list-group-item">Email : ${user.email}</li>
+          <li class="list-group-item">Gender : ${user.gender === 'male' ? 'Male': 'Female'}</li>
+          <li class="list-group-item">Birthday : ${user.birthday}</li>
+          <li class="list-group-item">Location : ${user.location.name}</li>
+        </ul>
+        <div class="card-body">
+        <a href="${user.link}" class="card-link" target="_blank">Go to Your Profile</a>
+      </div>
+     </div>
+      `;
+
+      document.getElementById('profile').innerHTML = profile;
+  }
+
+  //DOM Feed
+  function buildFeed(feed){
+      const data = feed.posts.data;
+      let posts = '<h2 class="text-center">Your Feed</h2>';
+      data.forEach(dat =>{
+        posts += `
+        <div class="card mb-3">
+                   <div class="card-header"><h4>${!dat.message ? 'Content not available😌' : dat.message}</h4></div>
+                   <small class="bg-primary p-2">${new Date(dat.created_time)}</small>
+               </div>
+    `;
+      })
+
+      document.getElementById('feed').innerHTML = posts;
+  }
+
+  //Event delegation 
+  document.querySelector('body').addEventListener('click',function(e){
+    
+    if(e.target.classList.contains('btn-danger')){
+        window.location.reload();
+    }
+
+  })
+
+  //Show alert
+  function showAlert(message){
+      const alert = document.getElementById('alert');
+      const div = document.createElement('div');
+      div.className = 'alert alert-info';
+      div.appendChild(document.createTextNode(message));
+
+      alert.appendChild(div);
+
+      setTimeout(() => alert.remove(),3000)
+      
+  }
+//   //Log out all
+//   function logout(){
+//     // FB.logout(function(response){
+//     //     window.location.reload()
+//     // //   setElements(false);
+      
+//     // });
+
+
+//   }
